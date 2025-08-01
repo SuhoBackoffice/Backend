dee41e6db12d65597e966bf230b8743c4c5249c0
KangBaekGwa <ksu9801@gmail.com>
2025-08-01 18:09:37 +0900

    feat: 의존성 설정 및 환경 분리 적용

---
+++ src/test/java/baekgwa/suhoserver/SuhoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package baekgwa.suhoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuhoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
