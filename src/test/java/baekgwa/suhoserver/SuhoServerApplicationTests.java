b025114d5ed189bd28e7b204d394386474b9c00c
KangBaekGwa <ksu9801@gmail.com>
2025-09-10 23:17:23 +0900

    test: S3FileClient Test 에서 모킹 진행

--- src/test/java/baekgwa/suhoserver/SuhoServerApplicationTests.java
+++ src/test/java/baekgwa/suhoserver/SuhoServerApplicationTests.java
@@ -2,9 +2,15 @@ package baekgwa.suhoserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import baekgwa.suhoserver.infra.upload.s3.S3FileClient;
 
 @SpringBootTest
 class SuhoServerApplicationTests {
+
+	@MockitoBean
+	private S3FileClient s3FileClient;
 
 	@Test
 	void contextLoads() {
