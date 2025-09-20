47cf7641600daa6223424350c98700b4190940a2
KangBaekGwa <ksu9801@gmail.com>
2025-09-20 16:52:56 +0900

    fix: material_inbound auto increment 누락 수정

--- src/main/resources/db/migration/V2.0.0__create_material_inbound.sql
+++ src/main/resources/db/migration/V2.0.0__create_material_inbound.sql
@@ -11,13 +11,13 @@
 
 CREATE TABLE `material_inbound`
 (
-    `id`             BIGINT       NOT NULL,
-    `project_id`     BIGINT       NOT NULL,
-    `drawing_number` VARCHAR(255) NOT NULL COMMENT '도번',
-    `item_name`      VARCHAR(255) NOT NULL COMMENT '품명',
-    `quantity`       BIGINT       NOT NULL COMMENT '입고 수량',
-    `created_at`     DATETIME     NOT NULL,
-    `modified_at`    DATETIME     NOT NULL,
+    `id`             BIGINT AUTO_INCREMENT NOT NULL,
+    `project_id`     BIGINT                NOT NULL,
+    `drawing_number` VARCHAR(255)          NOT NULL COMMENT '도번',
+    `item_name`      VARCHAR(255)          NOT NULL COMMENT '품명',
+    `quantity`       BIGINT                NOT NULL COMMENT '입고 수량',
+    `created_at`     DATETIME              NOT NULL,
+    `modified_at`    DATETIME              NOT NULL,
     PRIMARY KEY (`id`),
     CONSTRAINT `fk_material_inbound_project_id` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`)
 )
